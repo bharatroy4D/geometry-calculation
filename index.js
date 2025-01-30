5fec2a094628ca3441f082fd08c13049b87b9bee
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-01-30 22:09:50 +0600

    all cart section  ok

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-"" 
+// Triangle calculate
+function triangleCalculate() {
+    // triangle base
+    const triangleBaseField = document.getElementById('triangle-base');
+    const triangleBaseValue = triangleBaseField.value;
+    const base = parseFloat(triangleBaseValue);
+
+    // triangle height
+    const triangleHeightField = document.getElementById('triangle-height');
+    const triangleHeightValue = triangleHeightField.value;
+    const height = parseFloat(triangleHeightValue);
+
+
+    // Aria calculation
+    const aria = 0.5 * base * height;
+
+    // Aria value
+    const ariaValue = document.getElementById('aria-value');
+    ariaValue.innerText = aria;
+
+}
